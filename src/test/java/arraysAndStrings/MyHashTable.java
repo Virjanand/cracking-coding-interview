5621cd83dc14622934cb590cec715b452aad4599
Virjanand <virjanand@virjanand.com>
2021-01-03 10:34:23 +0100

    Refactor generics

--- src/test/java/arraysAndStrings/MyHashTable.java
+++ src/test/java/arraysAndStrings/MyHashTable.java
@@ -3,51 +3,52 @@ package arraysAndStrings;
 public class MyHashTable<K, V> {
 
     public static final int ARRAY_LENGTH = 2;
-    private Entry<K, V>[] entries = new Entry[ARRAY_LENGTH];
+    private final Entry<K, V>[] entries = new Entry[ARRAY_LENGTH];
 
     public V get(K key) {
-        Entry entry = entries[key.hashCode() % ARRAY_LENGTH];
+        Entry<K, V> entry = entries[key.hashCode() % ARRAY_LENGTH];
         while (entry != null && !entry.getKey().equals(key)) {
             entry = entry.getNext();
         }
         if (entry == null) {
             throw new IllegalArgumentException("No key found matching " + key);
         }
-        return (V) entry.getValue();
+        return entry.getValue();
     }
 
     public void put(K key, V value) {
         if (entries[key.hashCode() % ARRAY_LENGTH] != null) {
-            entries[key.hashCode() % ARRAY_LENGTH].setNext(new Entry(key, value));
+            entries[key.hashCode() % ARRAY_LENGTH].setNext(new Entry<>(key, value));
         } else {
             entries[key.hashCode() % ARRAY_LENGTH] = new Entry<>(key, value);
         }
     }
-}
 
-class Entry<K, V> {
-    private K key;
-    private V value;
-    private Entry next;
+    private static class Entry<K, V> {
+        private final K key;
+        private final V value;
+        private Entry<K, V> next;
 
-    public Entry(K key, V value) {
-        this.key = key;
-        this.value = value;
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        private K getKey() {
+            return key;
+        }
+
+        private V getValue() {
+            return value;
+        }
+
+        private void setNext(Entry<K, V> next) {
+            this.next = next;
+        }
+
+        private Entry<K, V> getNext() {
+            return next;
+        }
     }
 
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-
-    public void setNext(Entry next) {
-        this.next = next;
-    }
-
-    public Entry getNext() {
-        return next;
-    }
 }
