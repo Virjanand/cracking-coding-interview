2ef32f095f373d39fc84c6e95a03446ba690843b
Virjanand <virjanand@virjanand.com>
2021-01-15 07:01:00 +0100

    green test: more than 2 items with duplicate

--- src/main/java/chapter2LinkedLists/LinkedListDuplicateRemover.java
+++ src/main/java/chapter2LinkedLists/LinkedListDuplicateRemover.java
@@ -1,6 +1,8 @@
 package chapter2LinkedLists;
 
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class LinkedListDuplicateRemover {
     private final LinkedList<String> linkedList;
@@ -12,6 +14,9 @@ public class LinkedListDuplicateRemover {
     public void removeDuplicates() {
         if (linkedList.get(0).equals(linkedList.get(1))) {
             linkedList.remove(1);
+        }
+        if (linkedList.size() > 2 && linkedList.get(0).equals(linkedList.get(2))) {
+            linkedList.remove(2);
         }
     }
 }
