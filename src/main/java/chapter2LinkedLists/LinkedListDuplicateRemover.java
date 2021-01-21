af9c6d002f75b0148f83cf884564949d206dbc2c
Virjanand <virjanand@virjanand.com>
2021-01-21 07:00:45 +0100

    refactor: use own linkedList implementation to get to a proper implementation

--- src/main/java/chapter2LinkedLists/LinkedListDuplicateRemover.java
+++ src/main/java/chapter2LinkedLists/LinkedListDuplicateRemover.java
@@ -1,22 +1,30 @@
 package chapter2LinkedLists;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 
 public class LinkedListDuplicateRemover {
-    private final LinkedList<String> linkedList;
+    private final MyLinkedList<String> linkedList;
 
-    public LinkedListDuplicateRemover(LinkedList<String> linkedList) {
+    public LinkedListDuplicateRemover(MyLinkedList<String> linkedList) {
         this.linkedList = linkedList;
     }
 
-    public void removeDuplicates() {
-        if (linkedList.get(0).equals(linkedList.get(1))) {
-            linkedList.remove(1);
-        }
-        if (linkedList.size() > 2 && linkedList.get(0).equals(linkedList.get(2))) {
-            linkedList.remove(2);
+    public void removeDuplicatesInNTimeNSpace() {
+        MyLinkedListNode<String> currentNode = linkedList.getFirstNode();
+        Set<String> visitedElements = new HashSet<>();
+        while (currentNode != null) {
+            visitedElements.add(currentNode.getValue());
+            MyLinkedListNode<String> nextNode = currentNode.getNextNode();
+            if (nextNode == null) {
+                return;
+            }
+            if (visitedElements.contains(nextNode.getValue())) {
+                currentNode.setNextNode(nextNode.getNextNode());
+            } else {
+                visitedElements.add(nextNode.getValue());
+                currentNode = nextNode;
+            }
         }
     }
 }
--- src/test/java/chapter2LinkedLists/LinkedListDuplicateRemoverTest.java
+++ src/test/java/chapter2LinkedLists/LinkedListDuplicateRemoverTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
@@ -15,20 +15,28 @@ public class LinkedListDuplicateRemoverTest {
 
     private static Stream<Arguments> inputAndExpectedLinkedLists() {
         return Stream.of(
-                Arguments.of(new LinkedList<>(asList("a", "a")), new LinkedList<>(singletonList("a"))),
-                Arguments.of(new LinkedList<>(asList("a", "b")), new LinkedList<>(asList("a", "b"))),
-                Arguments.of(new LinkedList<>(asList("a", "b", "a")), new LinkedList<>(asList("a", "b")))
+                Arguments.of(createLinkedList(asList("a", "a")), createLinkedList(singletonList("a"))),
+                Arguments.of(createLinkedList(asList("a", "b")), createLinkedList(asList("a", "b"))),
+                Arguments.of(createLinkedList(asList("a", "b", "a")), createLinkedList(asList("a", "b"))),
+                Arguments.of(createLinkedList(asList("a", "b", "b", "a", "c")), createLinkedList(asList("a", "b", "c")))
         );
+    }
+
+    private static MyLinkedList<String> createLinkedList(List<String> nodes) {
+        MyLinkedList<String> result = new MyLinkedList<>();
+        for (String node : nodes) {
+            result.add(node);
+        }
+        return result;
     }
 
     @ParameterizedTest(name = "{0} -> {1}")
     @MethodSource("inputAndExpectedLinkedLists")
-    void removeDuplicatesInNTimeAndNSpace(LinkedList<String> inputLinkedList, LinkedList<String> expectedLinkedList) {
+    void removeDuplicatesInNTimeAndNSpace(MyLinkedList<String> inputLinkedList, MyLinkedList<String> expectedLinkedList) {
         LinkedListDuplicateRemover duplicateRemover = new LinkedListDuplicateRemover(inputLinkedList);
 
-        duplicateRemover.removeDuplicates();
+        duplicateRemover.removeDuplicatesInNTimeNSpace();
 
         assertThat(inputLinkedList).isEqualTo(expectedLinkedList);
     }
-
 }
